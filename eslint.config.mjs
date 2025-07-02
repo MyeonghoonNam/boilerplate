2344e92c5dec6b549961f99b4a36ae9e81e5a1c2
MyeonghoonNam <codenmh0822@naver.com>
2025-07-02 21:39:21 +0900

    config : boiler plate setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,26 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const prettierConfig = {
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  name: 'prettier',
+  rules: prettier.rules,
+};
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+  ),
+  prettierConfig,
+  {
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+      'tailwindcss/classnames-order': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
